fe9474999d291bf14837f6fd5c96b2faaf0421e4
Vĩ Bùi <vibuixuan2812@gmail.com>
2023-10-19 15:20:21 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Desktop_ClassWork3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnTao = new System.Windows.Forms.Button();
+            this.btnAn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(34, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(321, 372);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnTao
+            // 
+            this.btnTao.Location = new System.Drawing.Point(491, 82);
+            this.btnTao.Name = "btnTao";
+            this.btnTao.Size = new System.Drawing.Size(75, 23);
+            this.btnTao.TabIndex = 1;
+            this.btnTao.Text = "Tạo";
+            this.btnTao.UseVisualStyleBackColor = true;
+            this.btnTao.Click += new System.EventHandler(this.btnTao_Click);
+            // 
+            // btnAn
+            // 
+            this.btnAn.Location = new System.Drawing.Point(491, 129);
+            this.btnAn.Name = "btnAn";
+            this.btnAn.Size = new System.Drawing.Size(75, 23);
+            this.btnAn.TabIndex = 2;
+            this.btnAn.Text = "Cho Ăn";
+            this.btnAn.UseVisualStyleBackColor = true;
+            this.btnAn.Click += new System.EventHandler(this.btnAn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAn);
+            this.Controls.Add(this.btnTao);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnTao;
+        private System.Windows.Forms.Button btnAn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Cache;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Desktop_ClassWork3
+{
+    public partial class Form1 : Form
+    {
+        Meo meo1;
+        Dongvat[] thucung = new Dongvat[2];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTao_Click(object sender, EventArgs e)
+        {
+            thucung[0] = new Meo("Be meo", 2, true);
+            thucung[1] = new Ech("Chu ech xanh", 1, true);
+        }
+
+        private void btnAn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(thucung[0].An());
+            listBox1.Items.Add(thucung[1].An());
+        }
+
+    }
+    public class Dongvat
+    {
+        private string _name;
+        private int _age;
+        private bool _gender; //true is male; false is female
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { if (!(value < 0 || value > 200)) _age = value; }
+        }
+        public bool Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public Dongvat()
+        {
+            Name = string.Empty;
+            Age = 0;
+            Gender = true;
+        }
+        public Dongvat(string name_init, int age_init, bool gender_init)
+        {
+            Name = name_init;
+            Age = age_init;
+            Gender = gender_init;
+        }
+
+        public string Keu()
+        {
+            return "Động vật kêu";
+        }
+        public virtual string An()
+        {
+            return "Động vật ăn";
+        }
+    }
+    class Meo : Dongvat
+    {
+        private string _maulong;
+        public string Maulong { get => _maulong; set => _maulong = value; }
+        public override string An()
+        {
+            return $"Meo { this.Name} {this.Age} tuoi dang an";
+        }
+        public Meo(string name_meoput, int age_meoput, bool gender_meoput) : base(name_meoput,age_meoput,gender_meoput)
+        {
+            
+        }
+    }
+    class Ech : Dongvat
+    {
+        private string _mauda;
+        public string Mauda { get => _mauda; set => _mauda = value; }
+        public Ech(string name_echput, int age_echput, bool gender_echput) : base(name_echput, age_echput, gender_echput)
+        {
+
+        }
+        public Ech()
+        {
+
+        }
+        public override string An()
+        {
+            return $"Ech {this.Name} {this.Age} tuoi dang an";
+        }
+    }
+}
